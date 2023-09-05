9872abcb929258f30f39b9c188d073726b51a08e
yuseipen0716 <yuseipen1@gmail.com>
2023-09-06 00:54:00 +0900

    Add: install annotate gem, and run migrate

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id         :integer          not null, primary key
+#  name       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Group < ApplicationRecord
   has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  group_id   :integer          not null
+#  line_id    :text
+#
+# Indexes
+#
+#  index_users_on_group_id  (group_id)
+#
+# Foreign Keys
+#
+#  group_id  (group_id => groups.id)
+#
 class User < ApplicationRecord
   belongs_to :group
   has_many :expense_records
