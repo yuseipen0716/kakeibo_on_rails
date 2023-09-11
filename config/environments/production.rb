cefbc99de49abfae64080511d54efc81c47fdd4e
yuseipen0716 <yuseipen1@gmail.com>
2023-09-11 15:53:58 +0900

    add: Add secret key base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add secret key base
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 end
