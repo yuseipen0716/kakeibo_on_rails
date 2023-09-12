6ae337226848bb856e014a173dfb2f18b18770ea
yuseipen0716 <yuseipen1@gmail.com>
2023-09-12 23:38:40 +0900

    add: Tmp Implement handle message

--- app/service/message_handler.rb
+++ app/service/message_handler.rb
@@ -1,9 +1,25 @@
 class MessageHandler
+  BUILT_IN_MESSAGE = {
+    INPUT: '家計簿データ入力',
+    SHOW: '家計簿データ確認',
+    GROUP: 'グループ作成・参加',
+    HELP: 'ヘルプ'
+  }.freeze
   class << self
     # messageを受け取り、適切な処理に振り分け、replyを返す
     def perform(message)
-      # 一旦動作確認のため、messageをそのまま返す
-      return message
+      case message
+      when BUILT_IN_MESSAGE[:INPUT]
+        return 'にゅうりょく'
+      when BUILT_IN_MESSAGE[:SHOW]
+        return 'かくにん'
+      when BUILT_IN_MESSAGE[:GROUP]
+        return 'ぐるーぷ'
+      when BUILT_IN_MESSAGE[:HELP]
+        return 'へるぷ'
+      else
+        return '不明なメッセージ'
+      end
     end
   end
 end
