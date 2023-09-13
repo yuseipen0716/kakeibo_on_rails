0f1732580a20d2b36b945cd7e6cf91038bd66cbc
yuseipen0716 <yuseipen1@gmail.com>
2023-09-13 22:59:54 +0900

    add: Add model spec of Group

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  describe 'validation of name' do
+    let(:group) { Group.new(name: name) }
+    context 'name length within 10' do
+      let(:name) { '1234567890' }
+
+      it 'is valid with a name' do
+        expect(group).to be_valid
+      end
+    end
+
+    context 'name length is over 10' do
+      let(:name) { '12345678901' }
+
+      it 'is invalid with a name' do
+        expect(group).not_to be_valid
+      end
+    end
+  end
+end
