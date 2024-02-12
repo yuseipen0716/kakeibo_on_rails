9408edbb33be372d38ccb06a00a45fddce032111
yuseipen0716 <yuseipen1@gmail.com>
2024-02-12 16:50:43 +0900

    change: Disabled expense record is not included in the monthly total

--- app/usecases/get_monthly_total_usecase.rb
+++ app/usecases/get_monthly_total_usecase.rb
@@ -4,13 +4,14 @@ class GetMonthlyTotalUsecase
       # group機能は未実装なため、一旦user自体の家計簿データの合計を返す。
       total = if category && category != '合計'
                 ExpenseRecord.eager_load(:category)
+                             .active
                              .expense
                              .where(user:, transaction_date: period)
                              .where(categories: { name: category })
                              .sum(:amount)
               else
                 # 費目の指定がない場合は、userのExpenseRecordsの合計を返す
-                user.expense_records.expense.where(transaction_date: period).sum(:amount)
+                user.expense_records.active.expense.where(transaction_date: period).sum(:amount)
               end
       "#{total}円ナリ"
     end
--- spec/usecases/get_monthly_total_usecase_spec.rb
+++ spec/usecases/get_monthly_total_usecase_spec.rb
@@ -24,6 +24,27 @@ RSpec.describe GetMonthlyTotalUsecase, type: :usecase do
       end
     end
 
+    context '論理削除されているレコードが存在する場合' do
+      let(:category_name) { nil }
+
+      before do
+        create(
+          :expense_record,
+          amount: 1000,
+          transaction_date: Time.zone.now,
+          user:,
+          category: create(:category, name: '電気代'),
+          is_disabled: true # 論理削除済み
+        )
+      end
+
+      it '論理削除済みの家計簿データの分は合計に加算されない' do
+        # このcontextで追加したexpense_recordの1000円分は加算されないので、
+        # 合計は1500円になる。
+        expect(usecase).to eq('1500円ナリ')
+      end
+    end
+
     context 'categoryが指定されている場合' do
       context '指定したcategory（食費）の家計簿データが存在する場合' do
         let(:category_name) { create(:category, name: '食費').name }
@@ -38,6 +59,25 @@ RSpec.describe GetMonthlyTotalUsecase, type: :usecase do
 
         it '0円が返される' do
           expect(usecase).to eq('0円ナリ')
+        end
+      end
+
+      context '論理削除されているレコードが存在する場合' do
+        let(:category_name) { create(:category, name: '食費').name }
+
+        before do
+          create(
+            :expense_record,
+            amount: 1000,
+            transaction_date: Time.zone.now,
+            user:,
+            category: create(:category, name: '食費'),
+            is_disabled: true
+          )
+        end
+
+        it '今月の食費の合計金額が返される（論理削除済みの家計簿データ分は加算されない）' do
+          expect(usecase).to eq('1500円ナリ')
         end
       end
     end
